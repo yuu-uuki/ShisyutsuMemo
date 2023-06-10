4f5e42dad15ad92afd6900229539b0562143e07b
yuu-uuki <yuu99.331@gmail.com>
2023-06-10 12:36:33 +0900

    first commit

---
+++ ShisyutsuMemo/ShisyutsuMemo/ShisyutsuMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShisyutsuMemoApp.swift
+//  ShisyutsuMemo
+//
+//  Created by yuki.tanaka on 2023/06/10.
+//
+
+import SwiftUI
+
+@main
+struct ShisyutsuMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
